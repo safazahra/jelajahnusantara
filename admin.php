c667945792cf45cbb2428a739933653cad496c2a
safazahra <safanainaya12@gmail.com>
2026-01-08 16:28:20 +0700

    first commit

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Zona Jelajah Nusantara | Admin</title>
-    <link rel="icon" href="img/logo.png" />
+    <link rel="icon" href="img/logo.png"/>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
